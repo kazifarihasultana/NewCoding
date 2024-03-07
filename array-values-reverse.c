09543c13fde032a1a67e1c64a66aaeeb6bbca57e
Kazi Fariha Sultana <157713604+kazifarihasultana@users.noreply.github.com>
2024-03-07 06:30:58 +0200

    Add files via upload

---
+++ array-values-reverse.c
@@ -0,0 +1,27 @@
+// store data in an array and display in reverse
+
+#include<stdio.h>
+
+int main(){
+
+    int i, j, n;
+    float array[100];
+
+    printf("enter array size = ");
+    scanf("%d", &n);
+
+    printf("enter array values : \n");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%f", &array[i]);
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    
+
+}
